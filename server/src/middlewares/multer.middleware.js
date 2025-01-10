2bf210cce6cf5dfea222555e2fbb4f76637c62fc
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2025-01-11 04:40:10 +0500

    fix multer error

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,16 +1,20 @@
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb){
-        cb(null, './public/temp')
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
     },
-    filename:(req,file,cb)=>{
-        cb(null, file.originalname)
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
     }
-})
+  })
+  
+  export const upload = multer(
+     {
+       storage
+     }
+    )
 
-export const upload = multer({
-    storage
-});
 
   
